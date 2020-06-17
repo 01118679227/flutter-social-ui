78aad81d84b178b5cad7252757b4dc5e980fc29c
mahmoud <mh345abd@gmail.com>
2020-06-17 04:08:31 -0700

    commit

--- lib/ui.dart
+++ lib/ui.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
-class UI extends StatelessWidget {
+class UI extends StatefulWidget {
+  @override
+  UIState createState() {
+    return new UIState();
+  }
+}
+
+class UIState extends State<UI> {
+    int _isSelected = 0;
+  bool _selected = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,114 +59,130 @@ class UI extends StatelessWidget {
              ),
              SizedBox(height: 35.0,),
          Stack(
-            children : <Widget>[
-               Padding(
-             padding: const EdgeInsets.only(left: 30.0,right: 30.0),
-             child: Container(
-               //height: MediaQuery.of(context).size.height * .23,
-               width: MediaQuery.of(context).size.width ,
-               decoration: BoxDecoration(
-                 borderRadius: BorderRadius.circular(10.0),
-                 color: Colors.grey.shade200,
-               ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                      SizedBox(height: 20.0,),
-                     Text('Ac Service And Repair',
-                     style: TextStyle(
-                       color: Colors.black54,
-                       fontSize: 14.0,
-                      ),
-                     ),
-                      SizedBox(height: 8.0,),
-                      Padding(
-                        padding: const EdgeInsets.only(left:68.0),
-                        child: Text('Min SAR 45',
-                          style: TextStyle(
-                        color: Colors.cyan.shade600,
-                        fontSize: 12.0,
-                          ),
-                     ),
-                      ),
-                     SizedBox(height: 20.0,),
-                   ],
-                ),
-                 SizedBox(height: 30.0,),
-                Padding(
-                  padding: const EdgeInsets.only(left: 17.0,right: 13.0),
-                  child: Image.asset('image/Group 3019.png',height: 40.0,width: 40.0,),
-                ),
-                  ],
-                ),
-                
+              children : <Widget>[
+                 Padding(
+               padding: const EdgeInsets.only(left: 30.0,right: 30.0),
+               child: GestureDetector(
+                 onTap: (){
+                   setState(() {
+                     _selected = true;
+                   _isSelected = 1;
+                   });
+                 },
+                                child: Container(
+                   //height: MediaQuery.of(context).size.height * .23,
+                   width: MediaQuery.of(context).size.width ,
+                   decoration: BoxDecoration(
+                     borderRadius: BorderRadius.circular(10.0),
+                     color : (_selected == true && _isSelected == 1) ? Colors.blue.shade100 :Colors.grey.shade200,
+                   ),
+                    child: Row(
+         mainAxisAlignment: MainAxisAlignment.end,
+         children: <Widget>[
+           Column(
+             mainAxisAlignment: MainAxisAlignment.end,
+         children: <Widget>[
+             SizedBox(height: 20.0,),
+            Text('Ac Service And Repair',
+            style: TextStyle(
+              color: Colors.black54,
+              fontSize: 14.0,
              ),
-           ),
-            Padding(
-                 padding: const EdgeInsets.only(left: 15.0,top: 20.0),
-                 child: Container(
-                 height: 40.0,
-                 width: 40.0,
-                 decoration: BoxDecoration(
-                 borderRadius: BorderRadius.circular(50.0),
-                 color: Colors.grey.shade100,
+            ),
+             SizedBox(height: 8.0,),
+             Padding(
+                 padding: const EdgeInsets.only(left:68.0),
+                 child: Text('Min SAR 45',
+                   style: TextStyle(
+                 color: Colors.cyan.shade600,
+                 fontSize: 12.0,
+                   ),
+            ),
+             ),
+            SizedBox(height: 20.0,),
+          ],
+                    ),
+                     SizedBox(height: 30.0,),
+                    Padding(
+         padding: const EdgeInsets.only(left: 17.0,right: 13.0),
+         child: Image.asset('image/Group 3019.png',height: 40.0,width: 40.0,),
+                    ),
+         ],
+                    ),
+                    
+                 ),
                ),
-               child: Center(
-                 child: IconButton(icon: Icon(Icons.keyboard_arrow_left,color: Colors.black,),onPressed: (){},),
-                ),
+             ),
+              Padding(
+                   padding: const EdgeInsets.only(left: 15.0,top: 20.0),
+                   child: Container(
+                   height: 40.0,
+                   width: 40.0,
+                   decoration: BoxDecoration(
+                   borderRadius: BorderRadius.circular(50.0),
+                   color: Colors.grey.shade100,
                  ),
-                 ),
-             ],
-         ),
+                 child: Center(
+                   child: IconButton(icon: Icon(Icons.keyboard_arrow_left,color: Colors.black,),onPressed: (){},),
+                  ),
+                   ),
+                   ),
+               ],
+           ),
 
             SizedBox(height: 15.0,),
          Stack(
             children : <Widget>[
                Padding(
              padding: const EdgeInsets.only(left: 30.0,right: 30.0),
-             child: Container(
-               //height: MediaQuery.of(context).size.height * .23,
-               width: MediaQuery.of(context).size.width ,
-               decoration: BoxDecoration(
-                 borderRadius: BorderRadius.circular(10.0),
-                 color: Colors.grey.shade200,
-               ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Column(
-                  children: <Widget>[
-                      SizedBox(height: 20.0,),
-                     Text('Refrigerator Repair',
-                     style: TextStyle(
-                       color: Colors.black54,
-                       fontSize: 14.0,
-                      ),
-                     ),
-                      SizedBox(height: 8.0,),
-                      Padding(
-                        padding: const EdgeInsets.only(left:60.0),
-                        child: Text('Min SAR 60',
-                        style: TextStyle(
-                         color: Colors.cyan.shade600,
-                         fontSize: 12.0,
+             child: GestureDetector(
+                  onTap: (){
+                     setState(() {
+                     _selected = true;
+                   _isSelected = 2;
+                   });
+                  },
+                  child: Container(
+                 //height: MediaQuery.of(context).size.height * .23,
+                 width: MediaQuery.of(context).size.width ,
+                 decoration: BoxDecoration(
+                   borderRadius: BorderRadius.circular(10.0),
+                     color : (_selected == true && _isSelected == 2) ? Colors.blue.shade100 :Colors.grey.shade200,
+                 ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Column(
+                    children: <Widget>[
+                        SizedBox(height: 20.0,),
+                       Text('Refrigerator Repair',
+                       style: TextStyle(
+                         color: Colors.black54,
+                         fontSize: 14.0,
                         ),
-                     ),
-                      ),
-                     SizedBox(height: 20.0,),
-                   ],
-                ),
-                 SizedBox(height: 30.0,),
-                Padding(
-                  padding: const EdgeInsets.only(left: 28.0,right: 13.0),
-                  child: Image.asset('image/Group 3025.png',height: 40.0,width: 40.0,),
-                ),
-                  ],
-                ),
-                
+                       ),
+                        SizedBox(height: 8.0,),
+                        Padding(
+                          padding: const EdgeInsets.only(left:60.0),
+                          child: Text('Min SAR 60',
+                          style: TextStyle(
+                           color: Colors.cyan.shade600,
+                           fontSize: 12.0,
+                          ),
+                       ),
+                        ),
+                       SizedBox(height: 20.0,),
+                     ],
+                  ),
+                   SizedBox(height: 30.0,),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 28.0,right: 13.0),
+                    child: Image.asset('image/Group 3025.png',height: 40.0,width: 40.0,),
+                  ),
+                    ],
+                  ),
+                  
+               ),
              ),
            ),
             Padding(
--- screen_ui_5/README.md
+++
@@ -1,8 +0,0 @@
-# screen_ui_5
-
-A new Flutter project.
-
-## Getting Started
-
-For help getting started with Flutter, view our online
-[documentation](https://flutter.io/).
