bc1ee397b14f33517e449236fb1196250ba26057
mahmoud <mh345abd@gmail.com>
2020-06-18 04:53:10 -0700

    commit

--- lib/ui.dart
+++ lib/ui.dart
@@ -36,8 +36,8 @@ class UIState extends State<UI> {
                           color: Colors.grey.shade300,
                           ),
                         ),
-                        hintText: 'what are you looking',
-                        hintStyle: TextStyle(color: Colors.grey.shade300,fontSize: 12.0),
+                        hintText: 'عن ماذا تبحث',
+                        hintStyle: TextStyle(color: Colors.grey.shade300,fontSize: 15.0),
                       ),
                       ),
                 ),
@@ -54,9 +54,9 @@ class UIState extends State<UI> {
                  Align(
                    alignment: Alignment.centerRight,
                      child: Padding(
-                     padding: const EdgeInsets.only(right: 30.0,top: 30.0),
-                     child: Text('ELECTRICIAN SERVICES',
-                     style: TextStyle(color: Colors.blue.shade900,fontWeight: FontWeight.bold),
+                     padding: const EdgeInsets.only(right: 30.0,top: 20.0),
+                     child: Text('الخدمات الكهربائيه',
+                     style: TextStyle(color: Colors.blue.shade900,fontWeight: FontWeight.bold,fontSize: 15.0),
                      ),
                    ),
                  ),
@@ -93,7 +93,7 @@ class UIState extends State<UI> {
                           children: <Widget>[
                             
                               SizedBox(height: 20.0,),
-                              Text('Ac Service And Repair',
+                              Text('خدمة وإصلاح المكيفات',
                               style: TextStyle(
                                 color: Colors.black54,
                                 fontSize: 14.0,
@@ -102,7 +102,7 @@ class UIState extends State<UI> {
                               SizedBox(height: 8.0,),
                               Padding(
                                   padding: const EdgeInsets.only(left:68.0),
-                                  child: Text('Min SAR 45',
+                                  child: Text('منذ 45 دقيقه',
                                     style: TextStyle(
                                   color: Colors.cyan.shade600,
                                   fontSize: 12.0,
@@ -169,16 +169,19 @@ class UIState extends State<UI> {
                           Column(
                         children: <Widget>[
                             SizedBox(height: 20.0,),
-                           Text('Refrigerator Repair',
-                           style: TextStyle(
-                             color: Colors.black54,
-                             fontSize: 14.0,
-                            ),
+                           Padding(
+                             padding: const EdgeInsets.only(left: 38.0),
+                             child: Text('اصلاح الثلاجات',
+                             style: TextStyle(
+                               color: Colors.black54,
+                               fontSize: 14.0,
+                              ),
+                             ),
                            ),
                             SizedBox(height: 8.0,),
                             Padding(
                               padding: const EdgeInsets.only(left:60.0),
-                              child: Text('Min SAR 60',
+                              child: Text('منذ 60 دقيقه',
                               style: TextStyle(
                                color: Colors.cyan.shade600,
                                fontSize: 12.0,
@@ -239,21 +242,24 @@ class UIState extends State<UI> {
                         children: <Widget>[
                            Padding(
                         padding: const EdgeInsets.only(left: 28.0,right: 13.0),
-                        child: Image.asset('image/Group 3025.png',height: 40.0,width: 40.0,),
+                        child: Image.asset('image/Group 3022.png',height: 40.0,width: 40.0,),
                       ),
                           Column(
                         children: <Widget>[
                             SizedBox(height: 20.0,),
-                           Text('Refrigerator Repair',
-                           style: TextStyle(
-                             color: Colors.black54,
-                             fontSize: 14.0,
-                            ),
+                           Padding(
+                             padding: const EdgeInsets.only(left: 68.0),
+                             child: Text('غساله',
+                             style: TextStyle(
+                               color: Colors.black54,
+                               fontSize: 16.0,
+                              ),
+                             ),
                            ),
                             SizedBox(height: 8.0,),
                             Padding(
                               padding: const EdgeInsets.only(left:60.0),
-                              child: Text('Min SAR 60',
+                              child: Text('منذ 35 دقيقه',
                               style: TextStyle(
                                color: Colors.cyan.shade600,
                                fontSize: 12.0,
@@ -314,21 +320,24 @@ class UIState extends State<UI> {
                         children: <Widget>[
                            Padding(
                         padding: const EdgeInsets.only(left: 28.0,right: 13.0),
-                        child: Image.asset('image/Group 3025.png',height: 40.0,width: 40.0,),
+                        child: Image.asset('image/Group 3028.png',height: 40.0,width: 40.0,),
                       ),
                           Column(
                         children: <Widget>[
                             SizedBox(height: 20.0,),
-                           Text('Refrigerator Repair',
-                           style: TextStyle(
-                             color: Colors.black54,
-                             fontSize: 14.0,
-                            ),
+                           Padding(
+                             padding: const EdgeInsets.only(left: 38.0),
+                             child: Text('إصلاح أجهزة تنقية المياه',
+                             style: TextStyle(
+                               color: Colors.black54,
+                               fontSize: 14.0,
+                              ),
+                             ),
                            ),
                             SizedBox(height: 8.0,),
                             Padding(
-                              padding: const EdgeInsets.only(left:60.0),
-                              child: Text('Min SAR 60',
+                              padding: const EdgeInsets.only(left:100.0),
+                              child: Text('منذ 50 دقيقه',
                               style: TextStyle(
                                color: Colors.cyan.shade600,
                                fontSize: 12.0,
@@ -389,96 +398,24 @@ class UIState extends State<UI> {
                         children: <Widget>[
                            Padding(
                         padding: const EdgeInsets.only(left: 28.0,right: 13.0),
-                        child: Image.asset('image/Group 3025.png',height: 40.0,width: 40.0,),
+                        child: Image.asset('image/Group 3032.png',height: 40.0,width: 40.0,),
                       ),
                           Column(
                         children: <Widget>[
                             SizedBox(height: 20.0,),
-                           Text('Refrigerator Repair',
-                           style: TextStyle(
-                             color: Colors.black54,
-                             fontSize: 14.0,
-                            ),
-                           ),
-                            SizedBox(height: 8.0,),
-                            Padding(
-                              padding: const EdgeInsets.only(left:60.0),
-                              child: Text('Min SAR 60',
-                              style: TextStyle(
-                               color: Colors.cyan.shade600,
-                               fontSize: 12.0,
-                              ),
-                           ),
-                            ),
-                           SizedBox(height: 20.0,),
-                         ],
-                      ),
-                       SizedBox(height: 30.0,),
-                     
-                        ],
-                      ),
-                      
-                   ),
-                 ),
-               ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                      child: Padding(
-                       padding: const EdgeInsets.only(left: 15.0,top: 20.0),
-                       child: Container(
-                       height: 40.0,
-                       width: 40.0,
-                       decoration: BoxDecoration(
-                       borderRadius: BorderRadius.circular(50.0),
-                       color: Colors.grey.shade100,
-                     ),
-                     child: Center(
-                       child: IconButton(icon: Icon(Icons.keyboard_arrow_left,color: Colors.black,),onPressed: (){},),
-                      ),
-                       ),
-                       ),
-                ),
-                 ],
-             ),
-             SizedBox(height: 15.0,),
-             Stack(
-                children : <Widget>[
-                   Padding(
-                 padding: const EdgeInsets.only(left: 30.0,right: 30.0),
-                 child: GestureDetector(
-                      onTap: (){
-                         setState(() {
-                         _selected = true;
-                       _isSelected = 2;
-                       });
-                      },
-                      child: Container(
-                     //height: MediaQuery.of(context).size.height * .23,
-                     width: MediaQuery.of(context).size.width ,
-                     decoration: BoxDecoration(
-                       borderRadius: BorderRadius.circular(10.0),
-                         color : (_selected == true && _isSelected == 2) ? Colors.blue.shade100 :Colors.grey.shade200,
-                     ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
                            Padding(
-                        padding: const EdgeInsets.only(left: 28.0,right: 13.0),
-                        child: Image.asset('image/Group 3025.png',height: 40.0,width: 40.0,),
-                      ),
-                          Column(
-                        children: <Widget>[
-                            SizedBox(height: 20.0,),
-                           Text('Refrigerator Repair',
-                           style: TextStyle(
-                             color: Colors.black54,
-                             fontSize: 14.0,
-                            ),
+                             padding: const EdgeInsets.only(left: 38.0),
+                             child: Text('اصلاح الميكرويف',
+                             style: TextStyle(
+                               color: Colors.black54,
+                               fontSize: 14.0,
+                              ),
+                             ),
                            ),
                             SizedBox(height: 8.0,),
                             Padding(
                               padding: const EdgeInsets.only(left:60.0),
-                              child: Text('Min SAR 60',
+                              child: Text('منذ 45 دقيقه',
                               style: TextStyle(
                                color: Colors.cyan.shade600,
                                fontSize: 12.0,
@@ -516,6 +453,7 @@ class UIState extends State<UI> {
                  ],
              ),
              SizedBox(height: 15.0,),
+            
             ],
           ),
         ),
